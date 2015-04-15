74939214dcc9bc856c649f309cdbb7b59b1cf7c6
Reductio <github@timmjati.de>
2015-04-15 23:23:29 +0200

    encrypt_name and decrypt_name using pycrypto

--- cryptfuncs.py
+++ cryptfuncs.py
@@ -3,37 +3,54 @@ import sys
 from subprocess import Popen, PIPE
 from base64 import b64encode, b64decode
 
-e64 = lambda x: b64encode( x.encode( "utf-8" ), b"-_" )
-d64 = lambda x: b64decode( x.encode( "utf-8" ), b"-_" )
+import Crypto
+from Crypto.Hash.SHA256 import SHA256Hash
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+
+e64 = lambda x: b64encode( x, b"-_" )
+d64 = lambda x: b64decode( x, b"-_" )
+
+def sha256( byte_array ):
+	sha = SHA256Hash( data = byte_array )
+	return sha.digest()
 
 def hash_name( name ):
-	proc = Popen( ["openssl", "dgst", "-sha256", "-r"], stdin = PIPE, stdout = PIPE )
-	stdout, stderr = proc.communicate( name.encode( "utf-8" ) )
-	sha = stdout.decode( "utf-8" )
-	sha = sha.split( ' ' )[0]
+	sha = sha256( name.encode( "utf-8" ) )
 	return sha
+
+def create_counter_function( iv, init = 1 ):
+	bits = 128 - ( len( iv ) << 3 )
+	return Counter.new( bits, prefix = iv, initial_value = init, allow_wraparound = True )
 
 def encrypt_name( name, key ):
 	namehash = hash_name( name )
-	salt = namehash[:16]
-	iv = namehash[-32:]
+	iv = namehash[:8]
+	salt = namehash[8:16]
+	key = sha256( key + salt )
+
+	aes = AES.new( key, AES.MODE_CTR, counter = create_counter_function( iv ) )
+	encname = aes.encrypt( name )
+
 	saltiv = salt + iv
 	saltivb64 = e64( saltiv ).decode( "utf-8" )
-	proc = Popen( ["openssl", "aes-256-cbc", "-a", "-salt", "-S", salt, "-iv", iv, "-k", key], stdin = PIPE, stdout = PIPE )
-	stdout, stderr = proc.communicate( input = name.encode( "utf-8" ) )
-	stdout = stdout.decode( "utf-8" ).replace( "+", "-" ).replace( "/", "_" )
-	nname = stdout[:-1] + "." + saltivb64
-	return nname
+	encnameb64 = e64( encname ).decode( "utf-8" )
+
+	return encnameb64 + '.' + saltivb64
 
 def decrypt_name( name, key ):
-	name, saltivb64 = name.split( '.' )
-	name = name.replace( "-", "+" ).replace( "_", "/" ) + "\n"
-	saltiv = d64( saltivb64 )
-	salt = saltiv[:16]
-	iv = saltiv[-32:]
-	proc = Popen( ["openssl", "aes-256-cbc", "-a", "-d", "-k", key, "-S", salt, "-iv", iv], stdin = PIPE, stdout = PIPE )
-	stdout, stderr = proc.communicate( input = name.encode( "utf-8" ) )
-	dname = stdout.decode( "utf-8")
+	encname64, saltivb64 = name.split( '.' )
+
+	saltiv = d64( saltivb64.encode( "utf-8" ) )
+	encname = d64( encname64.encode( "utf-8" ) )
+
+	salt = saltiv[:8]
+	iv = saltiv[8:16]
+	key = sha256( key + salt )
+
+	aes = AES.new( key, AES.MODE_CTR, counter = create_counter_function( iv ) )
+	dname = aes.decrypt( encname ).decode( "utf-8" )
+
 	return dname
 
 def encrypt_file( file, out, keyfile ):
@@ -73,8 +90,6 @@ def convert_directory( source, dest, key, keyfile ):
 	print( "%s -> %s" % ( sourcename, encdest ) )
 
 def derive_new_key( keyfile ):
-	proc = Popen( ["openssl", "dgst", "-sha256", "-r", keyfile], stdout = PIPE )
-	stdout, _ = proc.communicate()
-	sha = stdout.decode( "utf-8" )
-	sha = sha.split( ' ' )[0]
-	return sha
+	with open( keyfile, "rb" ) as k:
+		keydata = k.read( 10240 )
+	return keydata
